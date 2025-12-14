63fc238ef1b60192ad29c324bed793d38c299cc6
askyellow <askyellow.nl@gmail.com>
2025-12-14 15:21:17 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -156,14 +156,19 @@ SQL_SEARCH_URL = os.getenv(
 app = FastAPI(title="YellowMind API")
 
 app.include_router(chat_router, prefix="/chat")
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=[
+        "https://askyellow.nl",
+        "https://www.askyellow.nl",
+        "http://localhost:5500",
+        "http://127.0.0.1:5500",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 @app.get("/chat")
 def serve_chat_page():
