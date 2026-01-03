0087ea38f7248f15ab8a23cff4dbf644eeff31c4
askyellow <askyellow.nl@gmail.com>
2026-01-03 17:56:02 +0100

    Delete core directory

--- core/lifespan.py
+++
@@ -1,9 +0,0 @@
-from contextlib import asynccontextmanager
-from app.core.startup import on_startup
-
-@asynccontextmanager
-async def lifespan(app):
-    # ⏳ startup
-    on_startup()
-    yield
-    # 🧹 shutdown (later, indien nodig)
