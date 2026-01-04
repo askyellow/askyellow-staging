e96809b892f533db42ed29de70c4a33104a901b7
askyellow <askyellow.nl@gmail.com>
2026-01-04 20:45:54 +0100

    Add files via upload

--- app/services/context.py
+++ app/services/context.py
@@ -1,6 +1,6 @@
 from app.identity_origin import try_identity_origin_answer
 from app.chat_engine.utils import search_sql_knowledge
-from app.knowledge.engine import match_question, KNOWLEDGE_ENTRIES
+from app.knowledge_engine import match_question, KNOWLEDGE_ENTRIES
 
 def build_context(question: str, language: str) -> dict:
     identity_answer = try_identity_origin_answer(question, language)
