a4971406c7c084fdaf033aaee23d240877487775
askyellow <askyellow.nl@gmail.com>
2026-01-07 11:38:19 +0100

    Add files via upload

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -5,9 +5,6 @@ from passlib.context import CryptContext
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-def verify_password(plain, hashed):
-    return pwd_context.verify(plain, hashed)
-
 import uuid
 
 router = APIRouter()
